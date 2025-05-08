0391fd5d47c4a8a69561c917615380bd579c4aaa
Heitor Rodrigues <heitor.rodrigues.savegnago@outlook.com>
2025-05-08 00:16:38 -0300

    6.11.2 - Pins for Mega Stack Shields 2020
    Added support for 2020 stacked version board;
    M0, M1 and Aux pins for LoRa module are now associated with the appropriate boards;
    If the board does not use those pin, an error is invoked during compilation;
    Small adjustments to the team logo drawing;

--- src/lib/pinos.h
+++ src/lib/pinos.h
@@ -13,7 +13,45 @@
 
 #ifdef USING_SHIELD
 
-#if USING_SHIELD == MEGA_OCTA_PTH_MK_I
+#if USING_SHIELD == MEGA_STACK_DADOS_ACIONAMENTO_2019
+
+#define IGN_1 36	/*act1*/
+#define IGN_2 61	/*act2*/
+#define IGN_3 46	/*act3*/
+#define IGN_4 55	/*act4*/
+
+#define HEAL_1 68	/*health 1*/
+#define HEAL_2 62	/*health 2*/
+#define HEAL_3 56	/*health 3*/
+#define HEAL_4 58	/*health 4*/
+
+#define buzzPin 54							//Pin that the buzzer is connected
+
+#define PWMout 13							//Pin that the LED who shows apogee state is connected
+
+// The LoRa Module in this board don't has M0, M1 or Aux
+
+#elif USING_SHIELD == MEGA_STACK_DADOS_ACIONAMENTO_2020
+
+#define IGN_1 36	/*act1*/
+#define IGN_2 61	/*act2*/
+#define IGN_3 46	/*act3*/
+#define IGN_4 55	/*act4*/
+
+#define HEAL_1 68	/*health 1*/
+#define HEAL_2 62	/*health 2*/
+#define HEAL_3 56	/*health 3*/
+#define HEAL_4 58	/*health 4*/
+
+#define buzzPin 22							//Pin that the buzzer is connected
+
+#define PWMout 13							//Pin that the LED who shows apogee state is connected
+
+#define M0_LORA_PIN 12 // Pinos adicionais do LoRa
+#define M1_LORA_PIN 11 // Pinos adicionais do LoRa
+#define AUX_LORA_PIN 58 // Pinos adicionais do LoRa
+
+#elif USING_SHIELD == MEGA_OCTA_PTH_MK_I
 
 #define IGN_1 54	/*act1*/
 #define IGN_2 56	/*act2*/
@@ -29,21 +67,9 @@
 
 #define PWMout 13							//Pin that the LED who shows apogee state is connected
 
-#elif USING_SHIELD == MEGA_STACK_DADOS_ACIONAMENTO_2019
-
-#define IGN_1 36	/*act1*/
-#define IGN_2 61	/*act2*/
-#define IGN_3 46	/*act3*/
-#define IGN_4 55	/*act4*/
-
-#define HEAL_1 68	/*health 1*/
-#define HEAL_2 62	/*health 2*/
-#define HEAL_3 56	/*health 3*/
-#define HEAL_4 58	/*health 4*/
-
-#define buzzPin 54							//Pin that the buzzer is connected
-
-#define PWMout 13							//Pin that the LED who shows apogee state is connected
+#define M0_LORA_PIN 12 // Pinos adicionais do LoRa
+#define M1_LORA_PIN 11 // Pinos adicionais do LoRa
+#define AUX_LORA_PIN 62 // Pinos adicionais do LoRa
 
 #else
 #error: Placa selecionada não está disponível
